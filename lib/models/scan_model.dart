4ec65853445f5b247292e1d96065708cf2fc883a
Karlos Aguirre <karlos.dev416@gmail.com>
2023-02-07 21:20:45 -0600

    Add these packages: flutter_barcode_scanner, path_provider, sqflite

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+// import 'package:meta/meta.dart';
+
+class ScanModel {
+  int? id;
+  String? tipo;
+  String valor;
+
+  ScanModel({
+    this.id,
+    this.tipo,
+    required this.valor,
+  }) {
+    if (valor.contains('http')) {
+      tipo = 'http';
+    } else {
+      tipo = 'geo';
+    }
+  }
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => new ScanModel(
+        id: json["id"],
+        tipo: json["tipo"],
+        valor: json["valor"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "tipo": tipo,
+        "valor": valor,
+      };
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_reader/pages/directions_page.dart';
 import 'package:qr_reader/pages/maps_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
 import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigation_bar.dart';
 import 'package:qr_reader/widgets/scan_button.dart';
@@ -30,6 +31,11 @@ class _HomePageBody extends StatelessWidget {
   Widget build(BuildContext context) {
     final uiProvider = Provider.of<UiProvider>(context);
     final currentIndex = uiProvider.selectedMenuOpt;
+    // final tempScan = ScanModel(valor: 'https://google.com');
+    // DBProvider.db.nuevoScan(tempScan);
+    // DBProvider.db.getScanById(1).then((scan) => print(scan?.valor));
+    // DBProvider.db.deleteAllScans().then(print);
+
     switch (currentIndex) {
       case 0:
         return MapsPage();
